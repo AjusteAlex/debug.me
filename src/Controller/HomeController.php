a75bc201609651ca5cbf76ad82675600a9c17450
tkimanh <tran.kimanh@orange.fr>
2023-12-12 17:29:47 +0100

    ajout de la navbar et du css du body

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class DemoController extends AbstractController
+class HomeController extends AbstractController
 {
-    #[Route('/demo', name: 'app_demo')]
+    #[Route('/', name: 'index_home')]
     public function index(): Response
     {
-        return $this->render('demo/index.html.twig', [
-            'controller_name' => 'DemoController',
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
         ]);
     }
 }
