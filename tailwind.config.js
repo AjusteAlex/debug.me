f71d5a1bca7fdb4e040afbec82c4be8a4202817f
tkimanh <tran.kimanh@orange.fr>
2024-01-10 13:08:22 +0100

    Ajout des variables personnalisées

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./assets/**/*.js",
-    "./templates/**/*.html.twig",
-  ],
+  content: ["./assets/**/*.js", "./templates/**/*.html.twig"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: (theme) => ({
+        "connexion-pattern": "url('/public/img/img_connexion.png')",
+      }),
+    },
+    colors: {
+      primary_color: "#1e1e1e",
+      pink: "#ff5555",
+      purple: "#4507a4",
+      white_transparent: "#f7f7f7d9",
+    },
+    fontFamily: {
+      serif: ["Lato"],
+    },
   },
   plugins: [],
-}
+};
