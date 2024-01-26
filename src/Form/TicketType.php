73903ec15fd3f8bef852431aa4ae2e6ae800dfb4
tkimanh <tran.kimanh@orange.fr>
2024-01-26 16:24:58 +0100

    [fix/front_creer_ticket] modification pour corriger le front

--- src/Form/TicketType.php
+++ src/Form/TicketType.php
@@ -7,6 +7,7 @@ use App\Entity\Ticket;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,21 +15,25 @@ class TicketType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $builder->add('title')
+        $builder->add('title', TextType::class, [
+            'label' => 'Titre',
+        ])
             ->add(
                 'content',
                 TextareaType::class,
                 [
-                    'attr' => ['class' => "rounded"]
+                    'attr' => ['class' => "rounded"],
+                    'label' => 'Contenu'
                 ]
             )
             ->add('tags', EntityType::class, [
                 'class' => Tag::class,
                 // uses the User.username property as the visible option string
                 'choice_label' => 'name',
-
+                'attr' => ['class' => "mr-6"],
                 // used to render a select box, check boxes or radios
-                'multiple' => true, // 'expanded' => true,
+                'multiple' => true, 'expanded' => true,
+                'label' => 'Tag(s)'
             ]);
     }
 
